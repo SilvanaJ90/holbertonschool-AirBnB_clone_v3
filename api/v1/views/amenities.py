ada112ae4798960ba03c95e256c1b3932164f8fb
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-19 14:44:22 -0500

    update-amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -7,28 +7,29 @@ from models.amenity import Amenity
 from api.v1.views import app_views
 from flask import request, jsonify, abort
 
+ignored = ['id', 'created_at', 'updated_at']
+
 
 @app_views.route('/amenities', strict_slashes=False,
                  methods=['GET', 'POST'])
 def all_amenity():
     if request.method == 'GET':
-        amenity_objects = storage.all(Amenity)
-        amenity_list = []
-        for key, val in amenity_objects.items():
-            amenity_list.append(val.to_dict())
-        return jsonify(amenity_list)
+        amenity= storage.all(Amenity)
+        amenities = []
+        for key, val in amenity.items():
+            amenities.append(val.to_dict())
+        return jsonify(amenities)
 
     if request.method == 'POST':
-        data = request.get_json(silent=True)
         amenity = Amenity()
-        if data is None:
+        if request.get_json(silent=True) is None:
             return 'Not a JSON', 400
-        if 'name' not in data.keys():
+        if 'name' not in request.get_json(silent=True).keys():
             return 'Missing name', 400
-        ignored_keys = ['id', 'created_at', 'updated_at']
-        for key in data:
-            if key not in ignored_keys:
-                setattr(amenity, key, data[key])
+
+        for key in request.get_json(silent=True):
+            if key not in ignored:
+                setattr(amenity, key, request.get_json(silent=True)[key])
         amenity.save()
         return(amenity.to_dict()), 201
     abort(404)
@@ -38,15 +39,15 @@ def all_amenity():
                  methods=['GET', 'DELETE', 'PUT'])
 def amenity_by_id(amenity_id):
     if request.method == 'GET':
-        amenity_objects = storage.all(Amenity)
-        for key, val in amenity_objects.items():
+        amenity = storage.all(Amenity)
+        for key, val in amenity.items():
             if val.id == amenity_id:
                 return val.to_dict()
         abort(404)
 
     if request.method == 'DELETE':
-        amenity_objects = storage.all(Amenity)
-        for key, val in amenity_objects.items():
+        amenity = storage.all(Amenity)
+        for key, val in amenity.items():
             if val.id == amenity_id:
                 storage.delete(val)
                 storage.save()
@@ -54,17 +55,16 @@ def amenity_by_id(amenity_id):
         abort(404)
 
     if request.method == 'PUT':
-        valid_request = request.get_json(silent=True)
-        if valid_request is None:
+        if request.get_json(silent=True) is None:
             return 'Not a JSON', 400
 
         ignored_keys = ['id', 'created_at', 'updated_at']
         amenity_objects = storage.all(Amenity)
         for key, val in amenity_objects.items():
             if val.id == amenity_id:
-                for key in valid_request:
+                for key in request.get_json(silent=True):
                     if key not in ignored_keys:
-                        setattr(val, key, valid_request[key])
+                        setattr(val, key, request.get_json(silent=True)[key])
                 storage.save()
                 return val.to_dict(), 200
         abort(404)
